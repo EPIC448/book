6b3e06f0999fcd9d550f07558f8c97d5b47de4a4
Sam. O <samrey2018@gmail.com>
2018-03-16 17:12:50 +0000

    files layout

---
+++ lib/book/book_scraper.rb
@@ -0,0 +1,5 @@
+class Book::book_scraper
+  attr_accessor :genre, :name, :book_description, :book_information
+
+
+end 
---
+++ lib/book/cli.rb
@@ -0,0 +1,15 @@
+class book::CLI
+
+   def book_genre_type
+
+   end
+
+end
+
+
+# Scrape the books.toscrape site.
+# A user should be presented with a list of Book Genres to select from.
+# Once a user selects a genre, they should then be presented with a list of book names from that genre.
+# The user should only see the first ten books, there should be an option for the user to either select from that list or see the next set of ten books.
+# A user can then select a book and be given the books name, the Product description, and its product information.
+# The user should be able to then see the book list again, start over or exit the gem.
