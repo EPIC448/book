ab484d35ac8f3fbc641d46c88519588166051d50
Sam. O <samrey2018@gmail.com>
2018-03-16 19:01:58 +0000

    added and option method to the cli and updated the book scraper.

--- lib/book/book_scraper.rb
+++ lib/book/book_scraper.rb
@@ -1,10 +1,18 @@
 class Book::Book_scraper
 
-  attr_accessor :genre, :name, :book_description
+  attr_accessor :genre, :name, :book_description, :url
 
-    def scraper
-      puts "This is where we scrape"
-
+    def self.scraper
+      doc = Nokogiri::HTML(open("http://books.toscrape.com"))
+       doc.css(".article.product_pod").map do |all_element|
+  binding.pry
+      read = self.new
+      read.genre
+      read.name
+      read.book_description
+      read.url
+      read
+     end
     end
 
 end
--- lib/book/cli.rb
+++ lib/book/cli.rb
@@ -2,14 +2,53 @@
 class Book::Cli
 
 
-    def list_books
-       @book_scraper = Book::Book_scraper.scraper
-       @book_scraper.each.with_index(1) do | list,i|
-          puts "#{i}. #{list.genre} - #{list.name} - #{list.book_description}"
+  def list_books
+    @book_scraper = Book::Book_scraper.scraper
+    @book_scraper.each.with_index(1) do | list,i|
+      puts "#{i}. #{list.genre} - #{list.name} - #{list.book_description} - #{list.url}"
     end
+  end
+
+  def option
+    input = nill
+
+
+    while input != "exit"
+
+       puts "Type Geners to view book geners"
+       puts "OR"
+        puts"Type more books to view the next 10 books"
+          puts "OR"
+             puts " Exit to Discountinue."
+                
+
+      input = gets.strip.downcase
+      i = input.to_i-1
+
+      if input.to_i > 0
+        list = @list[i] #line 19
+        puts "#{i}. #{list.genre} - #{list.name} - #{list.book_description} - #{list.url}"
+      elsif input = "Genres"
+        puts "list.genre"
+      elsif input ="more books"
+        puts "list the next 10 books"
+      elsif input = "exit"
+        goodbye
+      else
+        puts "not sure what you want"
+
+
+
+        "A user should be presented with a list of Book Genres to select from.
+        Once a user selects a genre, they should then be presented with a list of book names from that genre.
+        The user should only see the first ten books, there should be an option for the user to either select from that list or see the next set of ten books.
+        A user can then select a book and be given the books name, the Product description, and its product information.
+        The user should be able to then see the book list again, start over or exit the gem."
+      end
     end
 
-      def option
-          put "users choices"
-      end
-end
+    def goodbye
+      puts "see you tomorrow"
+
+    end
+  end
