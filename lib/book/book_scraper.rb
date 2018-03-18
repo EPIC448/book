c36a693e6a2e427a894d208d4c82cd783beefc89
Sam. O <samrey2018@gmail.com>
2018-03-18 03:41:47 +0000

    update scraper

--- lib/book/book_scraper.rb
+++ lib/book/book_scraper.rb
@@ -3,9 +3,9 @@ class Book::Book_scraper
   attr_accessor :genre, :name, :book_description, :url
 
     def self.scraper
+      doc = Nokogiri::HTML(open("http://books.toscrape.com/"))
       binding.pry
-      doc = Nokogiri::HTML(open("http://books.toscrape.com"))
-       doc.css(".article.product_pod").map do |all_element|
+       doc.search(".product_pod").map do |container|
       read = self.new
       read.genre
       read.name
