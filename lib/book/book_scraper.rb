5c6c723ab6ad107cac5a4d652285ae2efe41f454
Sam. O <samrey2018@gmail.com>
2018-03-19 18:51:53 +0000

    Scraper method working and updated

--- lib/book/book_scraper.rb
+++ lib/book/book_scraper.rb
@@ -3,17 +3,20 @@ class Book::Book_scraper
   attr_accessor :genre, :name, :book_description, :url
 
     def self.scraper
+
       doc = Nokogiri::HTML(open("http://books.toscrape.com/"))
-       doc.search(".product_pod").map do |container|
+       doc.search("div.page_inner").map do |container|
+        #  binding.pry
+
       read = self.new
-      binding.pry
-      # read.genre = doc.search(".")
-      # read.name = doc.search(".").text.strip
+      # read.genre = doc.search("li a").text
+      # read.name = doc.search("h3 a").text
       # read.book_description = doc.search(".").text
       # read.url = doc.search(".").text
-      # # price = p.price_color
-      read
+      # # price = doc.search("p.price_color").text
+      #read
      end
     end
+    #shift Q to get back in pry
 
 end
