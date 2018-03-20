14a2b73b45303efa27d07726a78560547f8c9cb1
Sam. O <samrey2018@gmail.com>
2018-03-20 19:42:37 +0000

    orgainzed scraper method

--- lib/book/book_scraper.rb
+++ lib/book/book_scraper.rb
@@ -1,23 +1,40 @@
 class Book::Book_scraper
 
   attr_accessor :genre, :name, :book_description, :product_information, :url
+  #shift Q to get back in pry
 
-    def self.scraper
+  # a method that iterater over book methods.
 
+   def self.collection
+      collection = [ ]
+      collection << self.scraper_homepage
+      collection << self.scraper_single_page
+      collection
+   end
+
+
+    def self.scraper_homepage
       doc = Nokogiri::HTML(open("http://books.toscrape.com/"))
-       doc.search("div.page_inner").map do |container|
-         binding.pry
-
+      doc.search("div.page_inner").map do |container|
       read = self.new
-      # read.genre = doc.search("li a").text
-      # read.name = doc.search("h3 a").text
-      # read.book_description = doc.search(".").text
-      #  read.product_information = doc.search (".").text
-      # read.url = "http://books.toscrape.com/"
-      # # price = doc.search("p.price_color").text
+      read.genre = doc.search("li a").text
+      read.name = doc.search("h3 a").text
+      read.url = "http://books.toscrape.com/"
+      read.price = doc.search("p.price_color").text
+      binding.pry
+      read
+     end
+    end
+
+    def self.scraper_single_page
+      doc = Nokogiri::HTML(open("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
+       doc.search("article.product_page").map do |container|
+      read = self.new
+    read.book_description = doc.search("p").text
+   read.product_information = doc.search ("tr").text
       #read
      end
     end
-    #shift Q to get back in pry
+
 
 end
