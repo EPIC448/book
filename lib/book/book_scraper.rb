26c81b48f6419b81ffb73b763d320e47074ca948
Sam. O <samrey2018@gmail.com>
2018-03-23 20:57:27 +0000

    refactorying

--- lib/book/book_scraper.rb
+++ lib/book/book_scraper.rb
@@ -19,26 +19,25 @@ class Book::Book_scraper
     doc = Nokogiri::HTML(open("http://books.toscrape.com"))
     list = []
     doc.search("ul.nav.nav-list ul a").map do |container|
-        list = self.new
-        list.genre = container.text.strip     #works perfect
-        binding.pry
-        list.url = container.values  #.#works perfect
-        list
-        url
+    list = self.new
+    genre = container.text.strip     #works perfect
+    url = container.values           #works perfect
+    list << {genre: genre, url: url}    #<Book::Book_scraper:0x000000027deb58 @genre="Mystery">
     end
+    list
   end
 
   # look at your student scraper method...
 
   def self.scrape_book(url)
     doc = Nokogiri::HTML(open(url))
-    list = [ ]
+    pages = [ ]
       doc.search("li.col-xs-6.col-sm-4.col-md-3.col-lg-3").map do |box|
-      list = self.new
-      list.book_description = box.css("h3 a").children.text
-      list.product_information = box.css("p.instock.availability").text
-      list.price = box.css("p.price_color").text
-      list
+      pages = self.new
+      pages.book_description = box.css("h3 a").children.text
+      pages.product_information = box.css("p.instock.availability").text
+      pages.price = box.css("p.price_color").text
+      pages
     end
   end
 end
