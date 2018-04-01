4a88ee86475e869c80d1e9f1e00c23683065962d
epic0448 <samuelogundiran@Yahoo.com>
2018-04-01 00:11:34 -0400

    lib/book/solo_book.rb

---
+++ lib/book/solo_book.rb
@@ -0,0 +1,12 @@
+
+class SoloBook
+attr_accessor :genre, :name, :book_description, :product_information, :price, :book_url
+#shift Q to get back in pry
+
+            @@all = []
+            
+            def initialize()
+
+            end
+
+end
