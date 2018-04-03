3a0a71e175332d7a66a47cb0bbf992923f99d747
epic0448 <samuelogundiran@Yahoo.com>
2018-04-02 22:51:05 -0400

    file moved around

--- lib/book.rb
+++
@@ -1,9 +0,0 @@
-require_relative './book/version'
-require_relative './book/cli'
-require_relative './book/book_scraper'
-
-# this is execution folder.
- require 'open-uri'
- require 'pry'
- require 'nokogiri'
-
