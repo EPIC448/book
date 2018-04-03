94a3100da7881820d797635b4221b31fca643cff
epic0448 <samuelogundiran@Yahoo.com>
2018-04-02 22:54:07 -0400

    new files

---
+++ book.rb
@@ -0,0 +1,9 @@
+require_relative './book/version'
+require_relative './book/cli'
+require_relative './book/book_scraper'
+
+# this is execution folder.
+ require 'open-uri'
+ require 'pry'
+ require 'nokogiri'
+
